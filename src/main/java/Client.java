d2fd74f97250a08d480b8a611be5a38f216922cd
Unknown <overskilers@gmail.com>
2018-11-26 23:18:19 +0000

    Updated user interface

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -43,88 +43,93 @@ public class Client {
 
         String readKeyboard = "";
         Scanner keyboardScanner = new Scanner(System.in);
+        String user_name = "";
 
         while (true) {
-            System.out.println("What do you want to do(inser the number):\n1.Login\n2.Logout\n3.Register\n4.Edit, add, or remove albuns,artist or musics\n5.Search for musics, artists or albuns\n6.Write a critique to a album\n7.Give editing privileges\n11.Upload a music\n12.Share musics\n13.Download a music");
+            System.out.println("What do you want to do(insert the number):\n1.Login\n2.Logout\n3.Register\n4.Edit, add, or remove albuns,artist or musics\n5.Search for musics, artists or albuns\n6.Write a critique to a album\n7.Give editing privileges\n11.Upload a music\n12.Share musics\n13.Download a music\n20.Create a playlist\n21.Remove a playlist\n22.Turn a playlist public/private\n23.Add music to a playlist");
             readKeyboard = keyboardScanner.nextLine();
             if (readKeyboard.matches(("1"))) {
-                String send = "1", username, password;
-                System.out.println("Insert your username:\n");
+                String send = "1_", username, password;
+                System.out.println("Insert your username:");
                 username = keyboardScanner.nextLine();
-                send.concat(username);
-                System.out.println("Insert your password:\n");
+                user_name = username;
+                send = send.concat(username).concat("_");
+                System.out.println("Insert your password:");
                 password = keyboardScanner.next();
-                send.concat(password).concat("sql");
+                send = send.concat(password);
+                System.out.println(send);
             }
             else if (readKeyboard.matches("2")) {
-                String send = "2", username;
-                System.out.println("Insert your username:\n");
+                String send = "2_", username;
+                System.out.println("Insert your username:");
                 username = keyboardScanner.nextLine();
-                send.concat(username).concat("sql");
+                send = send.concat(username);
             }
             else if (readKeyboard.matches("3")) {
-                String send = "3", username, password;
-                System.out.println("Insert your username:\n");
+                String send = "3_", username, password,first,last;
+                System.out.println("Insert your username:");
                 username = keyboardScanner.nextLine();
-                send.concat(username);
-                System.out.println("Insert your password:\n");
-                password = keyboardScanner.next();
-                send.concat("password").concat("sql");
+                send = send.concat(username).concat("_");
+                System.out.println("Insert your password:");
+                password = keyboardScanner.nextLine();
+                send = send.concat(password).concat("_");
+                System.out.println("First Name:");
+                first = keyboardScanner.nextLine();
+                send = send.concat(first).concat("_");
+                System.out.println("Last Name:");
+                last = keyboardScanner.nextLine();
+                send = send.concat(last);
             }
             else if (readKeyboard.matches("4")){
-                String send ="4", username,acao,tipo,name,caracteristica,edit,value,add;
+                String send ="4_",acao,tipo,name,caracteristica,edit,value,add;
                 boolean isBirth = false;
-                System.out.println("Insert your username:\n");
-                username = keyboardScanner.nextLine();
-                send.concat(username);
-                System.out.println("Do you want to alter, remove, or add\n");
+                System.out.println("Do you want to alter, remove, or add?");
                 acao = keyboardScanner.nextLine();
-                send.concat(acao);
+                send = send.concat(acao).concat("_");
                 if(acao.matches("add")){
-                    System.out.println("Artist,album or music?\n");
+                    System.out.println("Artist,album or music?");
                     tipo = keyboardScanner.nextLine();
-                    send.concat(tipo);
-                    System.out.println("Insert the name of the artist,album or music\n");
+                    send = send.concat(tipo).concat("_");
+                    System.out.println("Insert the name of the artist,album or music");
                     name = keyboardScanner.nextLine();
-                    send.concat(name);
-                    send.concat("sql");
+                    send = send.concat(name);
                 }
                 else if(acao.matches("alter")){
-                    System.out.println("Artist,album or music??\n");
+                    System.out.println("Artist,album or music?");
                     tipo = keyboardScanner.nextLine();
-                    send.concat(tipo);
+                    send = send.concat(tipo).concat("_");
                     if(tipo.matches("music")){
-                        System.out.println("What do you want to alter(name,year,lyrics,artist)?\n");
+                        System.out.println("What do you want to alter(name,year,lyrics,artist)?");
                         caracteristica = keyboardScanner.nextLine();
-                        send.concat(caracteristica);
+                        send = send.concat(caracteristica).concat("_");
                         if(caracteristica.matches("year")){
                             isBirth = true;
                         }
                     }
                     else if(tipo.matches("album")){
-                        System.out.println("What do you want to alter(name,year,description,artist,editor)?\n");
+                        System.out.println("What do you want to alter(name,year,description,artist,editor)?");
                         caracteristica = keyboardScanner.nextLine();
-                        send.concat(caracteristica);
+                        send = send.concat(caracteristica).concat("_");
                         if(caracteristica.matches("year")){
                             isBirth = true;
                         }
                     }
                     else if(tipo.matches("artist")){
-                        System.out.println("What do you want to alter(name,year,description?\n");
+                        System.out.println("What do you want to alter(name,year,description?");
                         caracteristica = keyboardScanner.nextLine();
-                        send.concat(caracteristica);
+                        send = send.concat(caracteristica).concat("_");
                         if(caracteristica.matches("year")){
                             isBirth = true;
                         }
                     }
 
-                    System.out.println("Which item is to be edited?:\n");
+                    System.out.println("Which item is to be edited?:");
                     edit = keyboardScanner.nextLine();
-                    send.concat(edit);
+                    send = send.concat(edit).concat("_");
 
-                    System.out.println("New value?:\n");
+                    System.out.println("New value?:");
                     value = keyboardScanner.nextLine();
-                    send.concat(edit);
+                    send = send.concat(value);
 
                     //Birth date needs to be checked for proper format
                     if (isBirth) {
@@ -135,53 +140,43 @@ public class Client {
                     }
                 }
                 else if(acao.matches("remove")){
-                    System.out.println("Remove a artist, album, or music?\n");
+                    System.out.println("Remove a artist, album, or music?");
                     tipo = keyboardScanner.nextLine();
-                    send.concat(tipo);
-                    System.out.println("Name?:\n");
+                    send = send.concat(tipo).concat("_");
+                    System.out.println("Name?:");
                     name = keyboardScanner.nextLine();
                     send.concat(name);
                 }
-                send.concat("sql");
             }
             else if(readKeyboard.matches("5")){
-                String send = "5", username,tipo,name;
-                System.out.println("Insert your username:\n");
-                username = keyboardScanner.nextLine();
-                send.concat(username);
-                System.out.println("Artist,album or music??\n");
+                String send = "5_",tipo,name;
+                System.out.println("Artist,album or music?");
                 tipo = keyboardScanner.nextLine();
-                send.concat(tipo);
-                System.out.println("Insert the name of what you want to search\n");
+                send = send.concat(tipo).concat("_");
+                System.out.println("Insert the name of what you want to search");
                 name = keyboardScanner.nextLine();
-                send.concat(name);
-                send.concat("sql");
+                send = send.concat(name);
             }
             else if(readKeyboard.matches("6")){
-                String send = "6", username,name,critique;
-                System.out.println("Insert your username:\n");
-                username = keyboardScanner.nextLine();
-                send.concat(username);
-                System.out.println("Insert the name of the album\n");
+                String send = "6_",name,critique,rating;
+                System.out.println("What is the critique?");
+                critique = keyboardScanner.nextLine();
+                send = send.concat(critique).concat("_");
+                System.out.println("What is the rating?");
+                rating = keyboardScanner.nextLine();
+                send = send.concat(rating).concat("_");
+                System.out.println("Insert the name of the album");
                 name = keyboardScanner.nextLine();
                 send.concat(name);
-                System.out.println("What is the critique?\n");
-                critique = keyboardScanner.nextLine();
-                send.concat(critique);
-                send.concat("sql");
             }
             else if(readKeyboard.matches("7")){
-                String send = "7", username,target;
-                System.out.println("Insert your username:\n");
-                username = keyboardScanner.nextLine();
-                send.concat(username);
+                String send = "7_",target;
                 System.out.println("Insert the targer username\n");
                 target = keyboardScanner.nextLine();
-                send.concat(target);
-                send.concat("sql");
+                send = send.concat(target);
             }
             else if(readKeyboard.matches("11")){
-                String send = "7", username,music;
+                String send = "11", username,music;
                 System.out.println("Insert your username:\n");
                 username = keyboardScanner.nextLine();
                 send.concat(username);
@@ -196,17 +191,15 @@ public class Client {
                 }
                 String address = ip.getHostAddress();
                 send.concat(address);
-                send.concat("sql");
             }
             else if(readKeyboard.matches("12")){
                 String send = "12", username,target;
-                System.out.println("Insert your username:\n");
+                System.out.println("Insert your username:");
                 username = keyboardScanner.nextLine();
                 send.concat(username);
-                System.out.println("Insert the targer username\n");
+                System.out.println("Insert the targer username");
                 target = keyboardScanner.nextLine();
                 send.concat(target);
-                send.concat("sql");
             }
             else if(readKeyboard.matches("13")){
                 String send = "13", username,music;
@@ -224,7 +217,32 @@ public class Client {
                 }
                 String address = ip.getHostAddress();
                 send.concat(address);
-                send.concat("sql");
+            }
+            else if(readKeyboard.matches("20")){
+                String send = "20_",name;
+                System.out.println("What is the playlist name");
+                name = keyboardScanner.nextLine();
+                send = send.concat(name);
+            }
+            else if(readKeyboard.matches("21")){
+                String send = "21_",name;
+                System.out.println("What is the playlist name");
+                name = keyboardScanner.nextLine();
+                send = send.concat(name);
+            }
+            else if(readKeyboard.matches("22")){
+                String send = "22_",name;
+                System.out.println("What is the playlist name");
+                name = keyboardScanner.nextLine();
+                send = send.concat(name);
+            }else if(readKeyboard.matches("22")){
+                String send = "22_",playlist_name,music_name;
+                System.out.println("What is the playlist name");
+                playlist_name = keyboardScanner.nextLine();
+                send = send.concat(playlist_name).concat("_");
+                System.out.println("What is the music name");
+                music_name = keyboardScanner.nextLine();
+                send = send.concat(music_name);
             }
         }
     }
